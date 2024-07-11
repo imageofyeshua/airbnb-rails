2f62eb35afbd2769ef51943ebe9cbf50e2576f0f
Daniel Park <intheimageofyeshua@gmail.com>
2024-07-11 22:01:43 +0900

    Average Rating with Counter Cache

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :property
+  belongs_to :property, counter_cache: true
 
   validates :content, presence: :true
   validates :cleanliness_rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
@@ -20,6 +20,9 @@ class Review < ApplicationRecord
       location_rating +
       value_rating
     
-      update_column(:final_rating, total_points.to_f / 6)
+    update_column(:final_rating, total_points.to_f / 6)
+
+    property.update_average_rating
   end
+
 end
