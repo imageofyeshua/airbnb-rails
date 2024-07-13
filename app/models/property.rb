2b42a8084d4bf31dc92a9fad63d67c3580f38488
Daniel Park <intheimageofyeshua@gmail.com>
2024-07-13 22:01:52 +0900

    Reservation Date Function Wrapup

--- app/models/property.rb
+++ app/models/property.rb
@@ -29,4 +29,23 @@ class Property < ApplicationRecord
 
     wishlisted_users.include?(user)
   end
+
+  def available_dates
+    next_reservation = reservations.upcoming_reservations.first
+    current_reservation = reservations.current_reservations.first
+
+    if current_reservation.nil? && next_reservation.nil?
+      # 1. Next nil and current -> nil
+      Date.tomorrow.strftime('%e %b')..(Date.tomorrow + 30.days).strftime('%e %b')
+    elsif current_reservation.nil?
+      # 2. 4. Next available and current -> nil
+      Date.tomorrow.strftime('%e %b')..next_reservation.checkin_date.strftime('%e %b')
+    elsif next_reservation.nil?
+      # 3. Next nil and current -> available
+      current_reservation.checkout_date.strftime('%e %b')..(Date.tomorrow + 30.days).strftime('%e %b')
+    else
+      # 5. Next available and current -> available
+      current_reservation.checkout_date.strftime('%e %b')..next_reservation.checkin_date.strftime('%e %b')
+    end 
+  end
 end
