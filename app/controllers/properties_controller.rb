d1c9d23cec3139f4cf472c6a2bd944d7995d7307
Daniel Park <intheimageofyeshua@gmail.com>
2024-07-15 22:26:56 +0900

    Added Overall Rating Function

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,12 @@
 class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
+    @overall_rating_counts = @property
+                              .reviews
+                              .group('ROUND(final_rating)')
+                              .count
+                              .transform_keys(&:to_i)
+    
+    @overall_rating_counts.default = 0
   end
 end
